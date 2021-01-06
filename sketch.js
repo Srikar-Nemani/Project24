87b050a3b93ca1af5cd0d7cdbbef6ba9cd07b59a
Srikar-Nemani <74164163+Srikar-Nemani@users.noreply.github.com>
2021-01-05 18:49:25 -0800

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,61 @@
+
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+const Body = Matter.Body;
+
+var log1,log2,log3,ball,ground;
+
+
+
+function setup() {
+	createCanvas(1200, 600);
+
+
+	engine = Engine.create();
+	world = engine.world;
+
+	//Create the Bodies Here.
+	ground = new Ground(600,570,1200,20);
+  	log1=new DustBin(1000,550,200,20);
+	log2=new DustBin(1090,490,20,100);
+  	log3=new DustBin(910,490,20,100);
+  	ball=new PaperBall(80,450,40);
+
+	Engine.run(engine);
+  
+}
+
+
+function draw() {
+  rectMode(CENTER);
+  background(0);
+  
+  Engine.update(engine);
+
+  
+ 
+
+  ground.display();
+  log1.display();
+  log2.display();
+  log3.display();
+  
+  ball.display();
+  
+  
+  drawSprites();
+ 
+}
+function keyPressed() {
+  if (keyDown === UP_ARROW) {
+    Matter.Body.applyForce(ball.body,ball.body.position,{x:80,y:-85});
+  }
+}
+
+	
+
+	
+
+
+
